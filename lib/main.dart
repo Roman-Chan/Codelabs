dc5c18163a7d5617063cd74c1615e27e7b2c9859
roman <roman.chan2622@gmail.com>
2023-01-11 20:24:57 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:my_awesome_name_app/pages/my_home_page.dart';
+import 'package:my_awesome_name_app/state/my_app_state.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/card_about.dart
@@ -0,0 +1,38 @@
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class CardAbout extends StatelessWidget {
+  const CardAbout({
+    Key? key,
+    required this.pair,
+  }) : super(key: key);
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+          child: MergeSemantics(
+            child: Wrap(
+              children: [
+                Text(
+                  '''Roman Ismael Chan Tuyu
+                  5a''',
+                  style: style.copyWith(fontSize: 20,fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
